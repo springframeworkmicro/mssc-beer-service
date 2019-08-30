8f58c5a96f99facb3f7cde669e404e907bdfb82d
santefia <santefia@gmail.com>
2019-08-30 07:28:13 -0400

    Added beer controller. closes #3

--- src/test/java/huby/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ src/test/java/huby/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -1,12 +1,11 @@
 package huby.springframework.msscbeerservice;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 //TODO: Add tests
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class MsscBeerServiceApplicationTests {
 
